4d506cf6484a8f974acf9fbf61723b5dcd62d191
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-07-19 05:51:54 -0800

    Reflaction

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Reflaction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type;
+            // 1
+            MyClass myClass = new MyClass();
+            type = myClass.GetType();
+            Console.WriteLine("MyClass.GetType()" + type);
+            
+
+            // 2
+            type = Type.GetType("Reflaction.MyClass");
+            Console.WriteLine("Type GetType()" + type);
+
+            // 3
+            type = typeof(MyClass);
+            Console.WriteLine("typeof(MyClass)" + type);
+
+            Console.ReadKey();
+
+        }
+    }
+}
